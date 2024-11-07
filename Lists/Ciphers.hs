61b8414e4f23576bd2cbcb15eac0ef86aee1b027
Vincent Onyango <me@verencelola.com>
2024-11-07 14:17:59 +0300

    Folding

--- Lists/Ciphers.hs
+++ Lists/Ciphers.hs
@@ -1,3 +1,39 @@
 module Cipher where
 
-import Data.Char
+import Data.Time
+
+data DatabaseItem = DbString String | DbNumber Integer | DbDate UTCTime deriving (Eq, Ord, Show)
+
+theDatabase :: [DatabaseItem]
+theDatabase = [DbDate (UTCTime (fromGregorian 1911 5 1) (secondsToDiffTime 34123)), DbNumber 9001, DbString "Hello, world!", DbDate (UTCTime (fromGregorian 1921 5 1) (secondsToDiffTime 43123))]
+
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate = foldr maybeCons []
+  where
+    maybeCons a b =
+      case a of
+        (DbDate date) -> date : b
+        _ -> b
+
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = foldr filterNumbers []
+  where
+    filterNumbers a b =
+      case a of
+        (DbNumber num) -> num : b
+        _ -> b
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = maximum . filterDbDate
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb = sum . filterDbNumber
+
+avgDb :: [DatabaseItem] -> Double
+avgDb x = fromIntegral (div (totalNumbers x) (lengthOfNumbers x))
+  where
+    totalNumbers =    sum . dbNumbers
+    lengthOfNumbers = fromIntegral . length . dbNumbers
+    dbNumbers = filterDbNumber
+
+
